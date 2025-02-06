4ed63a39bc83df208ca4cbdcef449216f227700b
viejop <158598912+viejop@users.noreply.github.com>
2025-02-06 11:41:16 -0500

    Create adress.py
    submitted full code

---
+++ adress.py
@@ -0,0 +1,143 @@
+mp_name = "Marcos Paulino"
+mp_age = 16.73
+mp_email = "267895@burlcoschools.org"
+mp_grade = 11
+mp_dlicense = False 
+
+sa_name = "Shahood Ahmad Alvi"
+sa_age = 17.08
+sa_email = "267741@burlcoschools.org"
+sa_grade = 11
+sa_dlicense = False
+
+rs_name = "Ryan Baxter Stiles"
+rs_age = 17.33
+rs_email = "267947@burlcoschools.org"
+rs_grade = 11
+rs_dlicense = True
+
+ar_name = "Aidan Matthew Rodriguez"
+ar_age = 17.17
+ar_email = "267922@burlcoschools.org"
+ar_grade = 11
+ar_dlicense = False
+
+gc_name = "Gavin Carnivale"
+gc_age = 17.04
+gc_email = "267764@burlcoschools.org"
+gc_grade = 11
+gc_dlicense = True
+
+ch_name = "Chris Hodson"
+ch_age = 17.12
+ch_email = "268359@burlcoschools.org"
+ch_grade = 11
+ch_dlicense = False
+
+nc_name = "Nathan Carn"
+nc_age = 16.98
+nc_email = "268021@burlcoschools.org"
+nc_grade = 11
+nc_dlicense = False
+
+lh_name = "Leo Hart"
+lh_age = 17.02
+lh_email = "268095@burlcoshools.org"
+lh_grade = 11
+lh_dlicense = True
+
+mb_name = "Mason Bush"
+mb_age = 17.08
+mb_email = "268262@burlcoschools.org"
+mb_grade = 11
+mb_dlicense = False
+
+md_name = "Muntag Diallo"
+md_age = 16.13
+md_email = "268154@burlcoschools.org"
+md_grade = 11
+md_dlicense = False
+
+jr_name = "Jaiden Rankus"
+jr_age = 16.44
+jr_email = "268188@burlcoshools.org"
+jr_grade = 11
+jr_dlicense = False
+
+print(" _________________________________________\n|////////////////////////////////////////||\n|//                                    \\\||\n|//  Name:",mp_name, "             \\\||")
+print("|//  Age:",mp_age, "                       \\\||")
+print("|//  Grade:",mp_grade, "                        \\\||")
+print("|//  License:",mp_dlicense, "                   \\\||")
+print("|//  Email:",mp_email, "  \\\||")
+print("|________________________________________||")
+print("")
+print(" _________________________________________\n|////////////////////////////////////////||\n|//                                    \\\||\n|//  Name:",sa_name, "         \\\||")
+print("|//  Age:",sa_age, "                       \\\||")
+print("|//  Grade:",sa_grade, "                        \\\||")
+print("|//  License:",sa_dlicense, "                   \\\||")
+print("|//  Email:",sa_email, "  \\\||")
+print("|________________________________________||")
+print("")
+print(" _________________________________________\n|////////////////////////////////////////||\n|//                                    \\\||\n|//  Name:",rs_name, "         \\\||")
+print("|//  Age:",rs_age, "                       \\\||")
+print("|//  Grade:",rs_grade, "                        \\\||")
+print("|//  License:",rs_dlicense, "                    \\\||")
+print("|//  Email:",rs_email, "  \\\||")
+print("|________________________________________||")
+print("")
+print(" _________________________________________\n|////////////////////////////////////////||\n|//                                    \\\||\n|//  Name:",ar_name, "    \\\||")
+print("|//  Age:",ar_age, "                       \\\||")
+print("|//  Grade:",ar_grade, "                        \\\||")
+print("|//  License:",ar_dlicense, "                   \\\||")
+print("|//  Email:",ar_email, "  \\\||")
+print("|________________________________________||")
+print("")
+print(" _________________________________________\n|////////////////////////////////////////||\n|//                                    \\\||\n|//  Name:",ch_name, "               \\\||")
+print("|//  Age:",ch_age, "                       \\\||")
+print("|//  Grade:",ch_grade, "                        \\\||")
+print("|//  License:",ch_dlicense, "                   \\\||")
+print("|//  Email:",ch_email, "  \\\||")
+print("|________________________________________||")
+print("")
+print(" _________________________________________\n|////////////////////////////////////////||\n|//                                    \\\||\n|//  Name:",nc_name, "                \\\||")
+print("|//  Age:",nc_age, "                       \\\||")
+print("|//  Grade:",nc_grade, "                        \\\||")
+print("|//  License:",nc_dlicense, "                   \\\||")
+print("|//  Email:",nc_email, "  \\\||")
+print("|________________________________________||")
+print("")
+print(" _________________________________________\n|////////////////////////////////////////||\n|//                                    \\\||\n|//  Name:",mb_name, "                 \\\||")
+print("|//  Age:",mb_age, "                       \\\||")
+print("|//  Grade:",mb_grade, "                        \\\||")
+print("|//  License:",mb_dlicense, "                   \\\||")
+print("|//  Email:",mb_email, "  \\\||")
+print("|________________________________________||")
+print("")
+print(" _________________________________________\n|////////////////////////////////////////||\n|//                                    \\\||\n|//  Name:",md_name, "              \\\||")
+print("|//  Age:",md_age, "                       \\\||")
+print("|//  Grade:",md_grade, "                        \\\||")
+print("|//  License:",md_dlicense, "                   \\\||")
+print("|//  Email:",md_email, "  \\\||")
+print("|________________________________________||")
+print("")
+print(" _________________________________________\n|////////////////////////////////////////||\n|//                                    \\\||\n|//  Name:",jr_name, "              \\\||")
+print("|//  Age:",jr_age, "                       \\\||")
+print("|//  Grade:",jr_grade, "                        \\\||")
+print("|//  License:",jr_dlicense, "                   \\\||")
+print("|//  Email:",jr_email, "   \\\||")
+print("|________________________________________||")
+print("")
+print(" _________________________________________\n|////////////////////////////////////////||\n|//                                    \\\||\n|//  Name:",gc_name, "            \\\||")
+print("|//  Age:",gc_age, "                       \\\||")
+print("|//  Grade:",gc_grade, "                        \\\||")
+print("|//  License:",gc_dlicense, "                    \\\||")
+print("|//  Email:",gc_email, "  \\\||")
+print("|________________________________________||")
+print("")
+print(" _________________________________________\n|////////////////////////////////////////||\n|//                                    \\\||\n|//  Name:",lh_name, "                   \\\||")
+print("|//  Age:",lh_age, "                       \\\||")
+print("|//  Grade:",lh_grade, "                        \\\||")
+print("|//  License:",lh_dlicense, "                    \\\||")
+print("|//  Email:",lh_email, "   \\\||")
+print("|________________________________________||")
+print("")
